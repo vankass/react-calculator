d0b10ce0a7f63d15c12e29bcb37f11a53406c44d
vankass <akk4aga@gmail.com>
2025-10-27 13:44:52 +0300

    feat: copy to clipboard

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
 import "./Display.css";
 export default function Display({value, onChange, handleKeyDown}) {
+  const [copied, setCopied] = useState(false);
+
+  async function handleCopy() {
+    if(!value) return;
+
+    try {
+      await navigator.clipboard.writeText(value);
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 1500);
+    } catch (err) {
+      console.error("Error", err);
+    }
+  }
+
   return (
-      <input type="text" value={value} className="display" onChange={onChange} onKeyDown={handleKeyDown} readOnly/>
+    <div className="display-wrapper">
+      <input type="text" value={value} className="display" onChange={onChange} onKeyDown={handleKeyDown} onClick={handleCopy} readOnly/>
+      {copied && <span className="copied">Copied!</span>}
+    </div>
   )
 }
